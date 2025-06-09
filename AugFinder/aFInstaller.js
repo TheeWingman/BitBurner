b4487d07deda7716e605b5a30ac07d9f44898179
TheeWingman <clayman130@gmail.com>
2025-06-09 14:37:07 -0400

    Add files via upload

---
+++ AugFinder/aFInstaller.js
@@ -0,0 +1,39 @@
+/**
+ * @param {String} command
+ * @return {Boolean}
+ **/
+export const inputTerminalCommand = (command) => {
+	const terminalInput = globalThis["document"].getElementById("terminal-input")
+	if (!terminalInput) {
+		ToastManager.instance.add("The terminal must be visible")
+	} else if (terminalInput.hasAttribute("disabled")) {
+		ToastManager.instance.add("The terminal must not be in use")
+	} else {
+		terminalInput.value = command
+		const handler = Object.keys(terminalInput)[1]
+		terminalInput[handler].onChange({ target: terminalInput })
+		terminalInput[handler].onKeyDown({ keyCode: 13, preventDefault: () => null })
+
+		return true
+	}
+
+	return false
+}
+
+
+/**
+ * @param {String[]} command
+ * @return {Boolean}
+ **/
+export const inputTerminalCommands = (command) => inputTerminalCommand(command.join("; "))
+
+/** @param {NS} ns */
+export async function main(ns) {
+  ns.rm("AugFinder/aF.txt");
+  await ns.wget("https://raw.githubusercontent.com/TheeWingman/BitBurner/448846977fef3de9dba6ab8f3e287f69d3cc8df9/AugFinder/aF.txt$0", "AugFinder/aF.txt");
+  const collection = JSON.parse(ns.read("AugFinder/aF.txt"));
+  for(const item of collection){
+    ns.write(item.filename, JSON.parse(item.fileContent),"w");
+  }
+  inputTerminalCommands(`alias aF="run AugFinder/findAugs.js"`);
+}
