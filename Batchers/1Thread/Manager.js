5f34d3c8dcc7316621970ec2444548c0985ec38a
TheeWingman <clayman130@gmail.com>
2025-06-14 10:14:20 -0400

    Add files via upload

--- Batchers/1Thread/Manager.js
+++ Batchers/1Thread/Manager.js
@@ -4,13 +4,17 @@ export async function main(ns) {
 	ns.disableLog('ALL');
 	ns.ui.openTail();
 	ns.ui.moveTail(300, 1300);
-	let srvrs = ns.args[2];
+	let srvrs = null;
 	const tgt = ns.args[0];
 	let servers = list_servers(ns).filter(s => ns.hasRootAccess(s)).filter(m => ns.getServerMaxRam(m) > 4);
 	let needsWG = false;
 	let noHS = false;
-	if (ns.args[1] == "noHS") {
+	let noPserv = false;
+	if (ns.args.includes("noHS")) {
 		noHS = true;
+	}
+	if (ns.args.includes("noPserv")) {
+		noPserv = true;
 	}
 
 
@@ -21,28 +25,31 @@ export async function main(ns) {
 		let tgtMonMax = ns.getServerMaxMoney(tgt);
 		if (srvrs == null) {
 			srvrs = servers;
-			while(true){
-			for (const srv of srvrs) {
-				ns.scp("/Batchers/remoteHack.js", srv, "home");
-				ns.scp("/Batchers/remoteWeak.js", srv, "home");
-				ns.scp("/Batchers/remoteGrow.js", srv, "home");
-				//ns.scp("repBoost.js", srv, "home");
-			}
+			while (true) {
 				srvrs = list_servers(ns).filter(s => ns.hasRootAccess(s));
+				for (const srv of srvrs) {
+					ns.scp("/Batchers/remoteHack.js", srv, "home");
+					ns.scp("/Batchers/remoteWeak.js", srv, "home");
+					ns.scp("/Batchers/remoteGrow.js", srv, "home");
+					//ns.scp("repBoost.js", srv, "home");
+				}
 				tgtSec = ns.getServerSecurityLevel(tgt);
 				tgtMon = ns.getServerMoneyAvailable(tgt);
 				if (tgtSec > tgtMinSec || tgtMon < tgtMonMax) {
 					needsWG = true;
 				}
+				else { needsWG = false;}
 				if (needsWG) {
-					while (tgtSec > tgtMinSec || tgtMon < tgtMonMax) {
-						srvrs = list_servers(ns).filter(s => ns.hasRootAccess(s));
-						for (const srv of srvrs) {
-							if (noHS && srv.includes("hacknet")) { }
-							else {
-								ns.print(`Ran pregame on ${srv}`);
-								ns.run("/Batchers/pregame.js", 1, tgt, srv);
-								await ns.sleep(20);
+					//while (tgtSec > tgtMinSec || tgtMon < tgtMonMax) {
+					srvrs = list_servers(ns).filter(s => ns.hasRootAccess(s));
+					for (const srv of srvrs) {
+						if (noHS && srv.includes("hacknet")) { }
+						else if (noPserv && srv.includes("pserv-")) { }
+						else {
+							ns.print(`Ran pregame on ${srv}`);
+							ns.run("/Batchers/pregame.js", 1, tgt, srv);
+							ns.ui.renderTail();
+							await ns.sleep(20);
 							}
 						}
 						ns.print("Waiting on Max $ & Min Sec");
@@ -52,18 +59,19 @@ export async function main(ns) {
 						tgtMon = ns.getServerMoneyAvailable(tgt);
 						tgtSec = ns.getServerSecurityLevel(tgt);
 					}
-					needsWG = false;
 					//ns.print("Done with pregame.js");
-				}
-				if (!needsWG) {
+				//}
+				else if (!needsWG) {
 					let loopCt = 0;
 					ns.clearLog();
 					loopCt++;
 					for (const srv of srvrs) {
 						if (noHS && srv.includes("hacknet")) { }
+						else if (noPserv && srv.includes("pserv-")) { }
 						else {
 							ns.print(`Ran remoteDeploy on ${srv}`);
 							ns.run("/Batchers/1Thread/remoteDeploy.js", 1, tgt, srv);
+							ns.ui.renderTail();
 							await ns.sleep(20);
 						}
 					}
--- Batchers/1Thread/remoteDeploy.js
+++ Batchers/1Thread/remoteDeploy.js
@@ -1,72 +1,110 @@
+import * as mod from "SphyxOS/util.js";
+
 /** @param {NS} ns */
 export async function main(ns) {
-  try {
-    let tgtSrv = ns.args[0];
-    let hackSvr = ns.args[1];
+  //try {
+  let tgtSrv = ns.args[0];
+  let hackSvr = ns.args[1];
 
-    ns.disableLog('ALL');
-    /*if (hackSvr == "pserv-24") {
-      ns.ui.openTail();
-    }*/
+  if (hackSvr == null) {
+    hackSvr = "pserv-0"
+  }
 
-    if (tgtSrv == null) {
-      tgtSrv = "joesguns";
-    }
-    if (hackSvr == null) {
-      hackSvr = "pserv-0";
-    }
-    //--------------------------------------
-    const hackRam = ns.getScriptRam("/Batchers/remoteHack.js", "home");
-    const growRam = ns.getScriptRam("/Batchers/remoteGrow.js", "home");
-    const weakRam = ns.getScriptRam("/Batchers/remoteWeak.js", "home");
+  ns.disableLog('ALL');
+  if (hackSvr == "pserv-0") {
+    //ns.ui.openTail();
+  }
 
-    let hackT = 0;
-    let growT = 0;
-    let weakTA = 0;
-    let weakTB = 0;
-    let possLoops = 0;
-    let maxRam = 0;
-    if (hackSvr.includes("hacknet")) {
-      let index = Number(hackSvr.split("r-")[1]);
-      //ns.tprint(`Node-${index}`);
-      maxRam = ns.hacknet.getNodeStats(index).ram;
-      //ns.tprint(`Node-${index} has ${(maxRam)}`);
-    }
-    else { maxRam = ns.getServerMaxRam(hackSvr); }
-    let maxMoney = ns.getServerMaxMoney(tgtSrv);
-    let timeWeak = ns.getWeakenTime(tgtSrv);
-    let timeGrow = ns.getGrowTime(tgtSrv);
-    let timeHack = ns.getHackTime(tgtSrv);
-    let growWait = timeWeak - timeGrow;
-    let hackWait = timeWeak - timeHack;
+  if (tgtSrv == null) {
+    tgtSrv = "foodnstuff";
+  }
+  //--------------------------------------
+  const hackRam = await mod.doGetScriptRam(ns, "/Batchers/remoteHack.js");
+  const growRam = await mod.doGetScriptRam(ns, "/Batchers/remoteGrow.js");
+  const weakRam = await mod.doGetScriptRam(ns, "/Batchers/remoteWeak.js");
 
-    growT = Math.ceil(ns.growthAnalyze(tgtSrv, (maxMoney/ (maxMoney - (ns.hackAnalyze(tgtSrv) * maxMoney))))) + 1;
+  let hackT = 0;
+  let growT = 0;
+  let weakTA = 0;
+  let weakTB = 0;
+  let possLoops = 0;
+  let maxRam = 0;
 
-    hackT = 1;
+  if (hackSvr.includes("hacknet")) {
+    let index = Number(hackSvr.split("r-")[1]);
+    //ns.tprint(`Node-${index}`);
+    maxRam = await mod.proxy(ns, "hacknet.getNodeStats", index).ram;
+    //ns.tprint(`Node-${index} has ${(maxRam)}`);
+  }
+  else { maxRam = await mod.getServerAvailRam(ns, hackSvr); }
+  let maxMoney = await mod.doGetServerMaxMoney(ns, tgtSrv);
+  let minSec = await mod.doGetServerMinSec(ns, tgtSrv);
+  let timeHack = await mod.getHckTimeBasic(ns, tgtSrv);
+  let timeWeak = timeHack * 4;
+  let timeGrow = timeHack * 3.2;
+  let growWait = timeWeak - timeGrow;
+  let hackWait = timeWeak - timeHack;
+  let hack1T = await mod.proxy(ns, "hackAnalyze", tgtSrv)
+  let afterHackMon = maxMoney - (maxMoney * hack1T);
+  //let srv = ns.getServer(tgtSrv);
+  //srv.moneyAvailable = afterHackMon
 
-    weakTA = Math.max(Math.ceil(ns.hackAnalyzeSecurity(hackT) / ns.weakenAnalyze(1)), 1);
+  hackT = 1;
 
-    weakTB = Math.max(Math.ceil(ns.growthAnalyzeSecurity(growT) / ns.weakenAnalyze(1)), weakTA, 1);
+  growT = await mod.getGrowThreads(ns, tgtSrv, afterHackMon, minSec);
 
-    possLoops = Math.floor(maxRam / ((growT * growRam) + (hackT * hackRam) + (weakTA * weakRam) + (weakTB * weakRam)));
+  weakTA = Math.ceil(await mod.proxy(ns, "hackAnalyzeSecurity", hackT) / await mod.weakenStr(ns));
 
-    ns.print(`
+  weakTB = Math.ceil(await mod.proxy(ns, "growthAnalyzeSecurity", growT) / await mod.weakenStr(ns));
+
+  possLoops = Math.floor(maxRam / ((growT * growRam) + (hackT * hackRam) + (weakTA * weakRam) + (weakTB * weakRam)));
+
+  ns.print(`
+    1 LOOP #s:
     possLoops: ${possLoops}
     growT: ${growT}
-    weakTA: ${weakTA}`)
+    weakTA: ${weakTA}
+    weakTB: ${weakTB}`)
 
-    if (possLoops > 0) {
-      if (possLoops > 100) { possLoops = 100; }
-      while (possLoops > 0) {
-        ns.exec("/Batchers/remoteHack.js", hackSvr, hackT, tgtSrv, hackWait);
-        ns.exec("/Batchers/remoteWeak.js", hackSvr, weakTA, tgtSrv);
-        ns.exec("/Batchers/remoteGrow.js", hackSvr, growT, tgtSrv, growWait);
-        ns.exec("/Batchers/remoteWeak.js", hackSvr, weakTB, tgtSrv);
-        --possLoops;
-      }
-    }
-    //--------------------------------------
-  } catch (error) {
-    ns.tprint(error);
+  if (possLoops > 0) {
+    if (possLoops > 100) { possLoops = 100; }
+    /*ns.print(`TOTALS:
+      hackT: ${hackT * possLoops}
+      weaakTA: ${weakTA * possLoops}
+      growT: ${growT * possLoops}
+      weakTB: ${weakTB * possLoops}`);*/
+
+    hackT *= possLoops;
+
+    afterHackMon = maxMoney - (maxMoney * (hack1T * possLoops));
+
+    growT = await mod.getGrowThreads(ns, tgtSrv, afterHackMon, await mod.proxy(ns, "getServerMinSecurityLevel", tgtSrv));
+
+    weakTA = Math.ceil(await mod.proxy(ns, "hackAnalyzeSecurity", hackT) / await mod.weakenStr(ns));
+
+    weakTB = Math.ceil(await mod.proxy(ns, "growthAnalyzeSecurity", growT) / await mod.weakenStr(ns));
+
+    ns.print(`TOTALS:
+    PossLoops: ${possLoops}
+    Hack: ${hackT}
+    WeakTA: ${weakTA}
+    Grow: ${growT}
+    WeakTB: ${weakTB}`)
+
+    /*ns.print(`Weak: ${timeWeak}`)
+    ns.print(`Grow: ${timeGrow}`)
+    ns.print(`Hack: ${timeHack}`)
+    ns.print(`growWait: ${growWait}`)
+    ns.print(`hackWait: ${hackWait}`)*/ 
+
+    ns.exec("/Batchers/remoteHack.js", hackSvr, hackT, tgtSrv, hackWait);
+    ns.exec("/Batchers/remoteWeak.js", hackSvr, weakTA, tgtSrv);
+    ns.exec("/Batchers/remoteGrow.js", hackSvr, growT, tgtSrv, growWait);
+    ns.exec("/Batchers/remoteWeak.js", hackSvr, weakTB, tgtSrv);
   }
+  //--------------------------------------
+  /*} catch (error) {
+    ns.print(error);
+    ns.ui.openTail();
+  }*/
 }
